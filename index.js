290637d4fac1aaf704360139ace028e259b5ab30
Harrison <hmseaborn@gmail.com>
2020-04-09 09:40:15 -0700

    init

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const server = require("./api/server.js");
+const server = require('./api/server.js');
 
-const port = 5000;
-server.listen(port, () => {
-  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
+const PORT = process.env.PORT || 5010;
+
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
 });
